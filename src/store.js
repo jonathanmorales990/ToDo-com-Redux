f9448e15bc8bd77deb51666843e05f2999aa6263
Jonathan Morales <johnmorales990@gmail.com>
2019-03-15 15:26:40 -0300

    Indentação

--- src/store.js
+++ src/store.js
@@ -1,12 +1,8 @@
 import todoReducer from './reducers/todo';
-
-
 import { combineReducers, createStore  } from 'redux';
 
 const reducers = combineReducers({
-    
     todoReducer: todoReducer
-
 });
 
 const store = createStore(reducers);
